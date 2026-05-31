bdaa4af1dd994aed3c535865d90e2bb0be2c8a69
cawzkf <camilafelixdosreis@gmail.com>
2026-05-31 17:55:16 -0400

    feat(infra): add EquipmentHistoryConfiguration

---
+++ GestaoEquipamentos.Infrastructure/Persistence/Configurations/EquipmentHistoryConfiguration.cs
@@ -0,0 +1,15 @@
+using GestaoEquipamentos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GestaoEquipamentos.Infrastructure.Persistence.Configurations;
+
+public class EquipmentHistoryConfiguration : IEntityTypeConfiguration<EquipmentHistory>
+{
+    public void Configure(EntityTypeBuilder<EquipmentHistory> builder)
+    {
+        builder.Property(h => h.Action)
+            .IsRequired()
+            .HasMaxLength(200);
+    }
+}
